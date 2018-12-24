d074a63455cef2f5c83dac6ded5a87afab33138e
nemanjan00 <nemanjan00@gmail.com>
2018-12-24 04:38:52 +0100

    Move config to external module

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// Load config defaults
-
-require("dotenv").config();
-
-const defaultConfig = require("../config.json");
-const config = require("./config");
-
-config.setDefaults(defaultConfig);
-
